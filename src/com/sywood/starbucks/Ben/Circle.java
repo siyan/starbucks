d9f8f55e9475438bdf3730bc3eb8e230a3048ad8
BenjaminBLi <StrikerB8@gmail.com>
2015-08-18 23:54:39 -0400

    commited

--- src/com/sywood/starbucks/Ben/Circle.java
+++ src/com/sywood/starbucks/Ben/Circle.java
@@ -3,14 +3,14 @@ package com.sywood.starbucks.Ben;
 import com.sywood.starbucks.siyan.AbstractShape;
 
 public class Circle extends AbstractShape {
-    private String name;
+    private String nam;
     private double r;
     public Circle(String CName, double rad){
-        name = CName;
+        nam = CName;
         r = rad;
     }
     public String name() {
-        return name;
+        return nam;
     }
 
     public double area() {
