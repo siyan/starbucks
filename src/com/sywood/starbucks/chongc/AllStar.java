28125e6e9462562286b5dbf7ea9f41806a8a3042
ALDJ <d301415926@126.com>
2015-08-31 17:25:06 +0800

    asdfghjkl

--- src/com/sywood/starbucks/chongc/AllStar.java
+++ src/com/sywood/starbucks/chongc/AllStar.java
@@ -87,7 +87,59 @@ public class AllStar {
 
 
     public static String stringClear(String str){
+        if(str.substring(0,1).equalsIgnoreCase(str.substring(1,2))){
+
+        }
 
          return null;
     }                    //
+
+
+
+    public static String noX(String str ){
+        if(str.substring(0,1).equalsIgnoreCase("x")) {
+            return noX(str.substring(1));
+        }else{
+            return str.substring(0,1) + noX(str.substring(1));
+        }
+    }
+
+    public static boolean array6(int[] array){
+        if(array.length!=0) {
+            return array[0] == 6 || array6(array);    //Wait, how do you summon the second to the last number in an array?
+        }else{
+            return false;
+        }
+    }
+
+    public static String changePi(String str){
+        if(str.length()<=0){
+            return null;
+        }else{
+            if(str.substring(0,2).equalsIgnoreCase("pi")){
+                return "3.14" + changePi(str.substring(2,str.length()));
+            }else{
+                return str.substring(0,1) + changePi(str.substring(1));
+            }
+        }
+    }
+
+    public static boolean nestParen(String str){
+        if(str.length()>=2) {
+            if (str.substring(0, 0) == "(" && str.substring(str.length()) == ")") {
+                return nestParen(str.substring(1, str.length() - 1));
+            } else {
+                return true;
+            }
+        }else{
+            return true;
+        }
+    }
+
+    public static String parenBit(String str){
+        //how will it be if str="(asd)asd(sdaw)"?
+        return null;
+    }
+
+
 }
