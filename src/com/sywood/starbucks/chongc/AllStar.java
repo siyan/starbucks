31672a36da38a50c8611e08ef2e29a8f3a47d079
ALDJ <d301415926@126.com>
2015-09-01 17:02:56 +0800

    asdfghjkl

--- src/com/sywood/starbucks/chongc/AllStar.java
+++ src/com/sywood/starbucks/chongc/AllStar.java
@@ -4,15 +4,15 @@ package com.sywood.starbucks.chongc;
  * Created by згёЦ on 2015/8/23.
  */
 public class AllStar {
-    public static String AllStar(String star,int sub){
-        if(sub==0){
-            return star.substring(0,1)+AllStar(star,sub+1);
-        }else {
-            if (sub == star.length()) {
-                return star.substring(star.length(), star.length());
-            } else {
-                return "*" +star.substring(sub,sub+1)+ AllStar(star, sub + 1);
-            }
+
+
+
+
+    public static String AllStar(String str){
+        if(str.length()<=0){
+            return null;
+        }else{
+            return str.substring(0,1)+"*"+AllStar(str.substring(1));
         }
     }
 
