c3eb793877ae37bf934c8771b8b53840442bb1be
BenjaminBLi <StrikerB8@gmail.com>
2015-09-03 22:17:04 -0400

    commited

--- src/com/sywood/starbucks/ben/Cards.java
+++ src/com/sywood/starbucks/ben/Cards.java
@@ -11,10 +11,13 @@ import java.nio.file.Paths;
 public class Cards {
 
     public static boolean isHigh(String name){
-        if (name == "king" || name == "queen" || name == "jack" || name == "ace"){
-            return true;
+        return (name.equals("king") || name.equals("queen") || name.equals("jack") || name.equals("ace"));
+    }
+    public static int player(int player){
+        if (player == 0){
+            return 1;
         }else{
-            return false;
+            return 2;
         }
     }
 
@@ -24,49 +27,71 @@ public class Cards {
         BufferedReader textReader = new BufferedReader(fr);
         int player1 = 0;
         int player2 = 0;
-        int count = 1;
-        int numLines = 52;
-        String[] cards = new String[numLines];
+        int[] count = {0, 0, 0};
+        String[] cards = new String[52];
 
-        for (int i = 0; i < numLines ; i++) {
+        for (int i = 0; i < 52 ; i++) {
             cards[i] = textReader.readLine();
+            textReader.readLine();
         }
         textReader.close();
-        for (int i = 0; i < numLines; i+=2) {
-            if (cards[i].equals("ace") && i <= 47 &&
-                    !isHigh(cards[i+1]) && !isHigh(cards[i+2]) && !isHigh(cards[i+3]) && !isHigh(cards[i+4])){
-                if ((i+1) % 4 == 1 || (i+1) % 4 == 0){
-                    player1 += 4;
-                    System.out.println("Player 1 earned 4 points");
-                }else{
-                    player2 += 4;
-                    System.out.println("Player 2 earned 4 points");
+        for (int i = 0; i < 52; i++) {
+            String card = cards[i];
+            if (isHigh(card)) {
+                count[2] = player(i % 2);
+                if (card.equals("ace")) {
+                    count[0] = 4;
+                    count[1] = 4;
+                } else if (card.equals("king")) {
+                    count[0] = 3;
+                    count[1] = 3;
+                } else if (card.equals("queen")) {
+                    count[0] = 2;
+                    count[1] = 2;
+                } else {
+                    count[0] = 1;
+                    count[1] = 1;
                 }
-            }else if (cards[i].equals("king") && i <= 48
-                    && !isHigh(cards[i+1]) && !isHigh(cards[i+2]) && !isHigh(cards[i+3])){
-                if ((i+1) % 4 == 1 || (i+1) %4 == 0){
-                    player1 += 3;
-                    System.out.println("Player 2 earned 3 points");
-                }else{
-                    player2 += 3;
-                    System.out.println("Player 1 earned 3 points");
-                }
-            }else if (cards[i].equals("queen") && i <= 49
-                    && !isHigh(cards[i+1]) && !isHigh(cards[i+2])){
-                if ((i+1) % 4 == 1 || (i+1) % 4 == 0){
-                    player1 += 2;
-                    System.out.println("Player 1 earned 2 points");
-                }else{
-                    player2 += 2;
-                    System.out.println("Player 2 earned 2 points");
-                }
-            }else if (cards[i].equals("jack") && i <= 50 && !isHigh(cards[i+1])){
-                if ((i+1) % 4 == 1 || (i+1) % 4 == 0){
-                    player1 += 1;
-                    System.out.println("Player 1 earned 1 points");
-                }else{
-                    player2 += 1;
-                    System.out.println("Player 2 earned 1 points");
+            }else if(count[0] > 0 && !isHigh(card)){
+                count[0] -= 1;
+                if (count[0] == 0){
+                    if (count[2] == 1){
+                        if(count[1] == 4){
+                            player1 += 4;
+                            count[0] = 0;
+                            count[1] = 0;
+                        }else if(count[1] == 3){
+                            player1 += 3;
+                            count[0] = 0;
+                            count[1] = 0;
+                        }else if(count[1] == 2){
+                            player1 += 2;
+                            count[0] = 0;
+                            count[1] = 0;
+                        }else if(count[1] == 1){
+                            player1 += 1;
+                            count[0] = 0;
+                            count[1] = 0;
+                        }
+                    }else{
+                        if(count[1] == 4){
+                            player2 += 4;
+                            count[0] = 0;
+                            count[1] = 0;
+                        }else if(count[1] == 3){
+                            player2 += 3;
+                            count[0] = 0;
+                            count[1] = 0;
+                        }else if(count[1] == 2){
+                            player2 += 2;
+                            count[0] = 0;
+                            count[1] = 0;
+                        }if(count[1] == 1){
+                            player2 += 1;
+                            count[0] = 0;
+                            count[1] = 0;
+                        }
+                    }
                 }
             }
         }
