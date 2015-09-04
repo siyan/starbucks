823a6789a8f97d52271bb31121b92725f22c1113
BenjaminBLi <StrikerB8@gmail.com>
2015-09-04 14:49:55 -0400

    commited

--- src/com/sywood/starbucks/ben/Cards.java
+++ src/com/sywood/starbucks/ben/Cards.java
@@ -1,12 +1,9 @@
 package com.sywood.starbucks.ben;
-//import javafx.scene.shape.Path;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.nio.file.Paths;
-
 
 public class Cards {
 
@@ -20,6 +17,19 @@ public class Cards {
             return 2;
         }
     }
+    public static int score(String card){
+        switch (card) {
+            case "ace":
+                return 4;
+            case "king":
+                return 3;
+            case "queen":
+                return 2;
+            case "jack":
+                return 1;
+        }
+        return 0;
+    }
 
     public static void main(String[] args) throws IOException{
         File f = new File("C:/Users/Striker/Downloads/card.in");
@@ -27,7 +37,9 @@ public class Cards {
         BufferedReader textReader = new BufferedReader(fr);
         int player1 = 0;
         int player2 = 0;
-        int[] count = {0, 0, 0};
+        int currPlayer = 0;
+        int turns = 0;
+        int cType = 0;
         String[] cards = new String[52];
 
         for (int i = 0; i < 52 ; i++) {
@@ -38,67 +50,19 @@ public class Cards {
         for (int i = 0; i < 52; i++) {
             String card = cards[i];
             if (isHigh(card)) {
-                count[2] = player(i % 2);
-                if (card.equals("ace")) {
-                    count[0] = 4;
-                    count[1] = 4;
-                } else if (card.equals("king")) {
-                    count[0] = 3;
-                    count[1] = 3;
-                } else if (card.equals("queen")) {
-                    count[0] = 2;
-                    count[1] = 2;
-                } else {
-                    count[0] = 1;
-                    count[1] = 1;
-                }
-            }else if(count[0] > 0 && !isHigh(card)){
-                count[0] -= 1;
-                if (count[0] == 0){
-                    if (count[2] == 1){
-                        if(count[1] == 4){
-                            player1 += 4;
-                            count[0] = 0;
-                            count[1] = 0;
-                            System.out.println("Player 1 earned " + 4 + " points");
-                        }else if(count[1] == 3){
-                            player1 += 3;
-                            count[0] = 0;
-                            count[1] = 0;
-                            System.out.println("Player 1 earned " + 3 + " points");
-                        }else if(count[1] == 2){
-                            player1 += 2;
-                            count[0] = 0;
-                            count[1] = 0;
-                            System.out.println("Player 1 earned " + 2 + " points");
-                        }else if(count[1] == 1){
-                            player1 += 1;
-                            count[0] = 0;
-                            count[1] = 0;
-                            System.out.println("Player 1 earned " + 1 + " points");
-                        }
+                currPlayer = player(i % 2);
+                turns = cType = score(card);
+            }else if(turns > 0 && !isHigh(card)){
+                turns -= 1;
+                if (turns == 0){
+                    if (currPlayer == 1){
+                        player1 += cType;
+                        System.out.println("Player 1 earned " + cType + " points");
+                        turns = cType = 0;
                     }else{
-                        if(count[1] == 4){
-                            player2 += 4;
-                            count[0] = 0;
-                            count[1] = 0;
-                            System.out.println("Player 2 earned " + 4 + " points");
-                        }else if(count[1] == 3){
-                            player2 += 3;
-                            count[0] = 0;
-                            count[1] = 0;
-                            System.out.println("Player 2 earned " + 3 + " points");
-                        }else if(count[1] == 2){
-                            player2 += 2;
-                            count[0] = 0;
-                            count[1] = 0;
-                            System.out.println("Player 2 earned " + 2 + " points");
-                        }if(count[1] == 1){
-                            player2 += 1;
-                            count[0] = 0;
-                            count[1] = 0;
-                            System.out.println("Player 2 earned " + 1 + " points");
-                        }
+                        player2 += cType;
+                        System.out.println("Player 2 earned " + cType + " points");
+                        turns = cType = 0;
                     }
                 }
             }
