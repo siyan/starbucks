dc543f60e17c0e863f7681a5f9ff35fb35b8848d
BenjaminBLi <StrikerB8@gmail.com>
2015-11-15 18:01:19 -0500

    commited

--- src/com/sywood/starbucks/ben/b2.java
+++ src/com/sywood/starbucks/ben/b2.java
@@ -1,26 +1,25 @@
 package com.sywood.starbucks.ben;
-
 import java.util.Scanner;
 
 
 public class b2{
     public static String reverse(String a){
-        String ret = "";
-        for (int i = a.length(); i >= 0; i--){
-            ret += a.charAt(i);
+        StringBuffer buf = new StringBuffer();
+        for (int i = a.length()-1; i >= 0; i--){
+            buf.append(a.charAt(i));
         }
-        return ret;
+        return buf.toString();
     }
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        for (int i = 0; i >= input.nextInt(); i++){
-            int a = Integer.parseInt(reverse(input.next()));
-            int b = Integer.parseInt(reverse(input.next()));
-            int c = a+b;
-            String d = String.valueOf(c);
-            d = reverse(d);
-            System.out.println(d);
+        int max = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < max; i++){
+            String[] ab = input.nextLine().split(" ");
+            int c = Integer.parseInt(reverse(ab[0])) + Integer.parseInt(reverse(ab[1]));
+            String d = reverse(String.valueOf(c));
+            int print = Integer.parseInt(d);
+            System.out.println(print);
         }
     }
 }
