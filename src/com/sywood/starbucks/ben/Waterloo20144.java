4a0bdd416fe563033dfd93fa40788d88e0f80954
BenjaminBLi <StrikerB8@gmail.com>
2015-11-22 18:27:26 -0500

    commited

---
+++ src/com/sywood/starbucks/ben/Waterloo20144.java
@@ -0,0 +1,17 @@
+package com.sywood.starbucks.ben;
+import java.util.Scanner;
+
+/**
+ * Created by Striker on 2015-11-21.
+ */
+public class Waterloo20144 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int rounds = Integer.parseInt(input.nextLine());
+        int m = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < rounds; i++) {
+
+        }
+    }
+}
---
+++ src/com/sywood/starbucks/ben/Waterloo20145.java
@@ -0,0 +1,36 @@
+package com.sywood.starbucks.ben;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Waterloo20145 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int num = input.nextInt();
+        ArrayList a = new ArrayList();
+        for (int i = 0; i < num; i++) {
+            a.add(input.next());
+        }
+        ArrayList b = new ArrayList();
+        for (int i = 0; i < num; i++) {
+            b.add(input.next());
+        }
+
+        if (a == b){
+            System.out.print("good");
+        }
+        else{
+            boolean equal = true;
+            for (int i = 0; i < num ; i++) {
+                int other = num - i-1;
+                if (!a.get(i).equals(b.get(other))){
+                    equal = false;
+                }
+            }if (equal){
+                System.out.print("good");
+            }else {
+                System.out.print("bad");
+            }
+        }
+    }
+}
