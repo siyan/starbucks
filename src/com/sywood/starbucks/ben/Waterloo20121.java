267c183bc9ec0d40c17d770b276da699a4e38b4d
BenjaminBLi <StrikerB8@gmail.com>
2015-12-14 22:30:07 -0500

    commited

--- src/com/sywood/starbucks/ben/Waterloo20121.java
+++ src/com/sywood/starbucks/ben/Waterloo20121.java
@@ -1,7 +1,27 @@
 package com.sywood.starbucks.ben;
+import java.util.Scanner;
 
-/**
- * Created by Striker on 2015-12-14.
- */
+
 public class Waterloo20121 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the speed limit: ");
+        int limit = input.nextInt();
+        System.out.print("Enter the recorded speed of the car: ");
+        int speed = input.nextInt();
+        if (speed >= limit){
+            System.out.print("Congratulations, you are within the speed limit!");
+        }else{
+            int diff = speed-limit;
+            int fine = 0;
+            if (diff <= 20){
+                fine = 100;
+            }else if (diff <= 30){
+                fine = 270;
+            }else{
+                fine = 500;
+            }
+            System.out.print("You are speeding and your fine is $" + String.valueOf(fine) + ".");
+        }
+    }
 }
