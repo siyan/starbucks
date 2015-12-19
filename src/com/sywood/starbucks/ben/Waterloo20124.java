b5e3df0d027934d1b237b7b1fd749abde978fc1c
BenjaminBLi <StrikerB8@gmail.com>
2015-12-18 21:42:49 -0500

    commited

---
+++ src/com/sywood/starbucks/ben/Waterloo20124.java
@@ -0,0 +1,16 @@
+package com.sywood.starbucks.ben;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Waterloo20124 {
+    public static void main(String[] args){
+        char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+        Scanner input = new Scanner(System.in);
+        int K = Integer.parseInt(input.nextLine());
+        String reverse = input.nextLine();
+
+        for (int i = 0; i < reverse.length(); i++) {
+        }
+
+    }
+}
