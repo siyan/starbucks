fd28f7b074897e2b1b3628cd5c714fa4ee1294b0
BenjaminBLi <StrikerB8@gmail.com>
2015-12-21 16:04:49 -0500

    commited

--- src/com/sywood/starbucks/ben/Waterloo2011/Waterloo20115.java
+++ src/com/sywood/starbucks/ben/Waterloo2011/Waterloo20115.java
@@ -1,8 +1,23 @@
 package com.sywood.starbucks.ben.Waterloo2011;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Waterloo20115 {
     public static void main(String[] args){
-
+        Scanner input = new Scanner(System.in);
+        int numPeo = Integer.parseInt(input.nextLine());
+        ArrayList<Integer> ways = new ArrayList(numPeo);
+        for (int i = 0; i < numPeo; i++) {
+            ways.add(1);
+        }
+        int tot = 0;
+        for (int i = 1; i < numPeo; i++) {
+            int person = input.nextInt();
+            ways.add(person, ways.get(person-1)*(1+ways.get(person)));
+        }
+        for (int num : ways){
+            tot += num;
+        }
+        System.out.print(tot);
     }
 }
