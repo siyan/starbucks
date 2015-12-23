9d31ee4d72a6d36d2024d0875438ec975d39a70b
BenjaminBLi <StrikerB8@gmail.com>
2015-12-23 13:25:16 -0500

    commited

--- src/com/sywood/starbucks/ben/Waterloo2011/Waterloo20114.java
+++ src/com/sywood/starbucks/ben/Waterloo2011/Waterloo20114.java
@@ -4,6 +4,73 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Waterloo20114 {
+    public static ArrayList<int[]> genArrayList(){
+        ArrayList<int[]> ret = new ArrayList<>();
+        for (int i = -1; i >-201; i--) {
+            for(int j = -200; j < 201; j++){
+                ret.add(new int[]{i, j});
+                if (i == -1 & j == 0 |
+                        i == -2 & j == 0|
+                        i == -3 & j == 0|
+                        i == -3 & j == 1|
+                        i == -3 & j == 2|
+                        i == -3 & j == 3|
+                        i == -4 & j == 3|
+                        i == -5 & j == 4|
+                        i == -5 & j == 5|
+                        i == -4 & j == 5|
+                        i == -3 & j == 5|
+                        i == -3 & j == 6|
+                        i == -3 & j == 7|
+                        i == -4 & j == 7|
+                        i == -5 & j == 7|
+                        i == -6 & j == 7|
+                        i == -7 & j == 7|
+                        i == -7 & j == 6|
+                        i == -7 & j == 5|
+                        i == -7 & j == 4|
+                        i == -7 & j == 3|
+                        i == -7 & j == 2|
+                        i == -7 & j == 1|
+                        i == -7 & j == 0|
+                        i == -7 & j == -6|
+                        i == -6 & j == -1|
+                        i == -5 & j == -1){
+                    ret.remove(ret.size()-1);
+                }
+            }
+        }
+        return ret;
+    }
+
+    public static int findIndex(int y, int x){
+        int index = 0;
+        boolean found = false;
+        ArrayList<int[]> block = genArrayList();
+        for (int[] coord : block){
+            if (!found){
+                if (coord[0] == y & coord[1] == x){
+                    found = true;
+                }else{
+                    index++;
+                }
+            }
+        }
+        return index;
+    }
+    public static boolean isIn(int y, int x) {
+        boolean found = false;
+        ArrayList<int[]> block = genArrayList();
+        for (int[] coord : block) {
+            if (!found) {
+                if (coord[0] == y & coord[1] == x) {
+                    found = true;
+                }
+            }
+        }
+        return found;
+    }
+
     public static int[] move(String dir, int val){
         if (dir.equals("u")){
             int[] ret = new int[]{0, 1};
@@ -21,40 +88,12 @@ public class Waterloo20114 {
     }
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        ArrayList<int[]> coords = new ArrayList();
+        ArrayList<int[]> coords = new ArrayList<>();
         for (int i = -1; i > -201; i--) {
             for (int j = -200; j < 201; j++){
                 coords.add(new int[]{i, j});
             }
         }
-        coords.remove(new int[]{-1, 0});
-        coords.remove(new int[]{-2, 0});
-        coords.remove(new int[]{-3, 0});
-        coords.remove(new int[]{-3, 1});
-        coords.remove(new int[]{-3, 2});
-        coords.remove(new int[]{-3, 3});
-        coords.remove(new int[]{-4, 3});
-        coords.remove(new int[]{-5, 3});
-        coords.remove(new int[]{-5, 4});
-        coords.remove(new int[]{-5, 5});
-        coords.remove(new int[]{-4, 5});
-        coords.remove(new int[]{-3, 5});
-        coords.remove(new int[]{-3, 6});
-        coords.remove(new int[]{-3, 7});
-        coords.remove(new int[]{-4, 7});
-        coords.remove(new int[]{-5, 7});
-        coords.remove(new int[]{-6, 7});
-        coords.remove(new int[]{-7, 7});
-        coords.remove(new int[]{-7, 6});
-        coords.remove(new int[]{-7, 5});
-        coords.remove(new int[]{-7, 4});
-        coords.remove(new int[]{-7, 3});
-        coords.remove(new int[]{-7, 2});
-        coords.remove(new int[]{-7, 1});
-        coords.remove(new int[]{-7, 0});
-        coords.remove(new int[]{-7, -1});
-        coords.remove(new int[]{-6, -1});
-        coords.remove(new int[]{-5, -1});
 
         String direction = input.next();
         int value = input.nextInt();
@@ -64,10 +103,10 @@ public class Waterloo20114 {
             while (value > 0) {
                 int row = point[1] + move[1]*value;
                 int col = point[0] + move[0]*value;
-                if (coords.contains(new int[]{row, col})){
+                if (isIn(row, col)){
                     System.out.println(col + " " + row + " " + "safe");
                     value -= 1;
-                    coords.remove(coords.indexOf(new int[]{row, col}));
+                    coords.remove(findIndex(row, col));
                 }else{
                     direction = "q";
                     value = 0;
