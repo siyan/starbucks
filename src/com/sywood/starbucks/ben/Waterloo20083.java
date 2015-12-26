7b74573cbdffa914beb8ea1a182ffe2e1ecba68c
BenjaminBLi <StrikerB8@gmail.com>
2015-12-26 18:09:04 -0500

    commited

--- src/com/sywood/starbucks/ben/Waterloo20083.java
+++ src/com/sywood/starbucks/ben/Waterloo20083.java
@@ -28,24 +28,14 @@ public class Waterloo20083 {
         map.add(new char[]{'M', 'N', 'O', 'P', 'Q', 'R'});
         map.add(new char[]{'S', 'T', 'U', 'V', 'W', 'X'});
         map.add(new char[]{'Y', 'Z', ' ', '-', '.', 'n'});
-        String toWrite = input.nextLine();
+        String toWrite = "A" + input.nextLine() + "n";
         int dist = 0;
         int[] start = find(map, 'A');
-        for (int i = 0; i < toWrite.length(); i++) {
-            if(i == 0) {
-                int[] curr = find(map, toWrite.charAt(i));
-                int[] next = find(map, toWrite.charAt(i + 1));
-                dist += Math.abs(start[0] - curr[0]) + Math.abs(start[1] - curr[1]) +
-                        Math.abs(next[0] - curr[0]) + Math.abs(next[1] - curr[1]);
-            } else if (i != toWrite.length()-1){
-                int[] curr = find(map, toWrite.charAt(i));
-                int[] next = find(map, toWrite.charAt(i+1));
-                dist += Math.abs(next[0] - curr[0]) + Math.abs(next[1] - curr[1]);
-            }else{
-                int[] curr = find(map, toWrite.charAt(i));
-                int[] last = find(map, 'n');
-                dist += Math.abs(last[0] - curr[0]) + Math.abs(last[1] - curr[1]);
-            }
+        for (int i = 0; i < toWrite.length()-1; i++) {
+            int[] curr = find(map, toWrite.charAt(i));
+            int[] next = find(map, toWrite.charAt(i+1));
+            dist += Math.abs(next[0] - curr[0]) + Math.abs(next[1] - curr[1]);
+
         }
         System.out.print(dist);
     }
