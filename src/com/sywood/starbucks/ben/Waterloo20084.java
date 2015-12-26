1eadc16dc3c3802dea1d704119c2c8b7c5b3dee7
BenjaminBLi <StrikerB8@gmail.com>
2015-12-26 13:09:35 -0500

    commited

--- src/com/sywood/starbucks/ben/Waterloo20084.java
+++ src/com/sywood/starbucks/ben/Waterloo20084.java
@@ -3,17 +3,5 @@ import java.util.Scanner;
 
 public class Waterloo20084 {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int max = 0;
-        int middle = 0;
-        for (int i = 0; i < 3; i++) {
-            int curr = Integer.parseInt(input.nextLine());
-            System.out.print(curr);
-            if (curr > max){
-                middle = max;
-                max = curr;
-            }
-        }
-        System.out.println(middle);
     }
 }
