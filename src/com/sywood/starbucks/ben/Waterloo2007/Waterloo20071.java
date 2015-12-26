991f897a824fa7fbcbbe82738cb4ea526971d0ad
BenjaminBLi <StrikerB8@gmail.com>
2015-12-26 13:19:50 -0500

    commited

--- src/com/sywood/starbucks/ben/Waterloo2007/Waterloo20071.java
+++ src/com/sywood/starbucks/ben/Waterloo2007/Waterloo20071.java
@@ -6,12 +6,17 @@ public class Waterloo20071 {
         Scanner input = new Scanner(System.in);
         int max = 0;
         int middle = 0;
+        int min = 990;
         for (int i = 0; i < 3; i++) {
             int curr = Integer.parseInt(input.nextLine());
-            System.out.print(curr);
             if (curr > max){
                 middle = max;
                 max = curr;
+            }else if (curr < min){
+                middle = min;
+                min = curr;
+            }else{
+                middle = curr;
             }
         }
         System.out.println(middle);
