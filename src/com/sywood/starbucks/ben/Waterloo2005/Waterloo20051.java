751bfa652792e4efdc2d657c84859d56274b0dc5
BenjaminBLi <StrikerB8@gmail.com>
2015-12-28 13:36:49 -0500

    commited

---
+++ src/com/sywood/starbucks/ben/Waterloo2005/Waterloo20051.java
@@ -0,0 +1,10 @@
+package com.sywood.starbucks.ben.Waterloo2005;
+import java.util.Scanner;
+
+public class Waterloo20051 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Number of daytime minutes?");
+        int day = Integer.parseInt(input.nextLine());
+    }
+}
---
+++ src/com/sywood/starbucks/ben/Waterloo2006/Waterloo20064.java
@@ -0,0 +1,10 @@
+package com.sywood.starbucks.ben.Waterloo2006;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Waterloo20064 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+    }
+}
