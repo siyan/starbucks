d926b4e1fc126a84127bea8879a303fff238092f
BenjaminBLi <StrikerB8@gmail.com>
2015-12-28 23:14:51 -0500

    commited

--- src/com/sywood/starbucks/ben/Waterloo2014/Waterloo20145.java
+++ src/com/sywood/starbucks/ben/Waterloo2014/Waterloo20145.java
@@ -1,8 +1,19 @@
 package com.sywood.starbucks.ben.Waterloo2014;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Collections;
 
 public class Waterloo20145 {
+    private static boolean isIn(ArrayList<String[]> pairs, String num, String num2){
+        for (String[] pair:pairs){
+            if (num.equals(pair[1]) & !num2.equals(pair[0]) |
+                    num.equals(pair[0]) & !num2.equals(pair[1])){
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
@@ -15,22 +26,19 @@ public class Waterloo20145 {
         for (int i = 0; i < num; i++) {
             b.add(input.next());
         }
-
-        if (a == b){
-            System.out.print("good");
-        }
-        else{
-            boolean equal = true;
-            for (int i = 0; i < num ; i++) {
-                int other = num - i-1;
-                if (!a.get(i).equals(b.get(other))){
-                    equal = false;
-                }
-            }if (equal){
-                System.out.print("good");
-            }else {
-                System.out.print("bad");
+        ArrayList<String[]> pairs = new ArrayList<>();
+        boolean good = true;
+        for (int i = 0; i < a.size(); i++){
+            if (!isIn(pairs, a.get(i), b.get(i))){
+                good = false;
+            }else{
+                pairs.add(new String[]{a.get(i), b.get(i)});
             }
+        }
+        if (good){
+            System.out.print("good");
+        }else{
+            System.out.print("bad");
         }
     }
 }
