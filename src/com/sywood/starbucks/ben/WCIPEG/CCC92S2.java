6647698fe15be824f3ec42b07f7d8dc6cbbbdda2
BenjaminBLi <StrikerB8@gmail.com>
2016-01-01 20:04:29 -0500

    commited

--- src/com/sywood/starbucks/ben/WCIPEG/CCC92S2.java
+++ src/com/sywood/starbucks/ben/WCIPEG/CCC92S2.java
@@ -3,6 +3,22 @@ import java.util.Scanner;
 
 public class CCC92S2 {
     public static void main(String[] args){
-
+        Scanner input = new Scanner(System.in);
+        int iters = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < iters; i++) {
+            int num = Integer.parseInt(input.nextLine());
+            int digit = num%10;
+            while (num > 99){
+                System.out.println(num);
+                num -= digit;
+                num = num/10;
+                digit = num % 10;
+            }
+            if (num % 11 == 0){
+                System.out.println("The number " + num + " is divisible by 11.");
+            }else{
+                System.out.println("The number " + num + " is not divisible by 11");
+            }
+        }
     }
 }
