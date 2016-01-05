175d850456c30d4895b6e9551b06679d5b31d9b6
BenjaminBLi <StrikerB8@gmail.com>
2016-01-04 23:43:04 -0500

    commited

---
+++ src/com/sywood/starbucks/ben/WCIPEG/CCC963.java
@@ -0,0 +1,13 @@
+package com.sywood.starbucks.ben.WCIPEG;
+import java.util.Scanner;
+
+public class CCC963 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int iter = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < iter; i++){
+            int iters = Integer.parseInt(input.nextLine());
+
+        }
+    }
+}
