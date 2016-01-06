d1e790db14696552979fd885db47d3747728bdf3
BenjaminBLi <StrikerB8@gmail.com>
2016-01-05 20:55:12 -0500

    commited

--- src/com/sywood/starbucks/ben/WCIPEG/CCC963.java
+++ src/com/sywood/starbucks/ben/WCIPEG/CCC963.java
@@ -6,8 +6,21 @@ public class CCC963 {
         Scanner input = new Scanner(System.in);
         int iter = Integer.parseInt(input.nextLine());
         for (int i = 0; i < iter; i++){
-            int iters = Integer.parseInt(input.nextLine());
+            int n = input.nextInt();
+            int k = input.nextInt();
+            String bits = "";
+            for (int j = 0; j < n; j++) {
+                if (--k > 0){
+                    bits += "1";
+                }else{
+                    bits += "0";
+                }
+            }
+            boolean complete = false;
+            while (!complete){
+                /*TODO: implement switch code for 1s and 0s*/
 
+            }
         }
     }
 }
