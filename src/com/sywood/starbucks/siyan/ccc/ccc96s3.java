f7a7743f20b6c7758a796a4a9769ae6c40f510c7
siyan <siyan@macbookair>
2016-01-06 19:54:31 -0500

    ccc 1996 C -- format

--- src/com/sywood/starbucks/siyan/ccc/ccc96s3.java
+++ src/com/sywood/starbucks/siyan/ccc/ccc96s3.java
@@ -10,13 +10,13 @@ public class ccc96s3 {
     private static int k;
     private static char[] A = new char[30];
 
-    private static void  printA(int i, int j) {
-        if (i == 0 && j == 0) {
+    private static void  printA( int i, int j ) {
+        if ( i == 0 && j == 0 ) {
             for ( int l = 0; l < n; l++ )  {
                 System.out.print( A[l] );
             }
 
-            System.out.println("");
+            System.out.println( "" );
         }
         else {
             if( j > 0 ) {
@@ -39,8 +39,8 @@ public class ccc96s3 {
 
         for( int i = 0; i < num; i++ ) {
             String[] pair = sc.nextLine().split(" ");
-            n = Integer.valueOf( pair[0]);
-            k = Integer.valueOf( pair[1]);
+            n = Integer.valueOf( pair[0] );
+            k = Integer.valueOf( pair[1] );
             System.out.println( "The bit patterns are"  );
             printA( n, k );
         }
