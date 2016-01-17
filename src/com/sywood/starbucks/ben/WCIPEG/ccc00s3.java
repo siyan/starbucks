b250cbabd72853102ffeaf1581cfc29841106b4b
BenjaminBLi <StrikerB8@gmail.com>
2016-01-16 23:56:12 -0500

    commited

---
+++ src/com/sywood/starbucks/ben/WCIPEG/ccc00s3.java
@@ -0,0 +1,13 @@
+package com.sywood.starbucks.ben.WCIPEG;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class ccc00s3 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int url = input.nextInt();
+        ArrayList<String> urls = new ArrayList<>(url);
+        int count = url*2;
+
+    }
+}
--- src/com/sywood/starbucks/siyan/ccc/ccc00j5.java
+++ src/com/sywood/starbucks/siyan/ccc/ccc00j5.java
@@ -8,18 +8,36 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.ArrayList
 import java.util.Map;
 
 /**
  * Created by siyan on 16-01-16.
  */
 public class ccc00j5 {
+    private static ArrayList<String> process(String line){
+        ArrayList<String> ret = new ArrayList<>();
+        for (int i = 0; i < line.length(); i++){
+            int startPos = line.indexOf("HREF=")+1<line.length()-1 ? line.indexOf("HREF=")+1 : -1;
+            int endPos = line.indexOf("\"", startPos+1) < line.length()-1 ? line.indexOf("\"", startPos+1):startPos;
+            ret.add(line.substring(startPos, endPos));
+        }
+        return ret;
+    }
+    private static boolean scan(Map<String, ArrayList<String>> links, String url){
+        boolean ret = false;
+        for (String key : links.keySet()){
+            for (ArrayList<String> link : links.get(key)){
+
+            }
+        }
+    }
 
     public static void main(String[] args) {
         String fileName = "data/surf.in";
         int lineCounter = 0;
         int numPages = 0;
-        Map<String, String> links = new LinkedHashMap<String, String>();
+        Map<String, ArrayList<String>> links = new LinkedHashMap<>();
         String url = null;
         try ( BufferedReader br = new BufferedReader( new FileReader( fileName ) ) ) {
             for(String line; (line = br.readLine()) != null; ) {
@@ -34,10 +52,10 @@ public class ccc00j5 {
                         lineCounter = 0;
                 }
                 else if( line.contains("HREF")) {
-                    int startPos = line.indexOf( "HREF=") + 6;
+                    int startPos = line.indexOf( "HREF=") + 1;
                     int endPos = line.indexOf("\"", startPos + 1 );
                     String linkUrl = line.substring( startPos, endPos );
-                    links.put( url, linkUrl );
+                    links.put( url,  process(line));
                     System.out.println( "Link from " + url + " to " + linkUrl );
                 }
 
@@ -54,6 +72,7 @@ public class ccc00j5 {
                 //System.out.println( line );
             }
         }
+
         catch (IOException e) {
             e.printStackTrace();
         }
