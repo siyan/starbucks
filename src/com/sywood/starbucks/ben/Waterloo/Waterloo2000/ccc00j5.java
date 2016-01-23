905779275aaae84791c179b53588670b37586d6c
BenjaminBLi <StrikerB8@gmail.com>
2016-01-22 22:22:26 -0500

    commited

---
+++ src/com/sywood/starbucks/ben/Waterloo/Waterloo2000/ccc00j5.java
@@ -0,0 +1,62 @@
+package com.sywood.starbucks.ben.Waterloo.Waterloo2000;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ccc00j5 {
+    private static ArrayList<String> findLinks(String url){
+        ArrayList<String> links = new ArrayList<>();
+        for (int i = 0; i < url.length(); i++) {
+            int start = url.indexOf("HREF=") + 6;
+            int end = url.indexOf("\"", start);
+            String link = url.substring(start, end);
+            links.add(link);
+            System.out.println("Link from " + url + " to " + link);
+        }
+        return links;
+    }
+    private static String scanPage(){
+        Scanner input = new Scanner(System.in);
+        String ret = "";
+        String line = input.nextLine();
+        while (!line.equalsIgnoreCase("</HTML>")){
+            ret += line;
+            line = input.nextLine();
+        }
+        return ret;
+    }
+    private static boolean scan(String two, ArrayList<String> links){
+        for (String link : links){
+            if (links.equals(two)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int pages = input.nextInt();
+        HashMap<String, ArrayList<String>> paths = new HashMap<>();
+        while (pages > 0){
+            String url = input.nextLine();
+            paths.put(url, findLinks(scanPage()));
+        }
+        String one = input.nextLine();
+        String two = input.nextLine();
+        while(!one.equalsIgnoreCase("The End") & !two.equalsIgnoreCase("The End")){
+            if (scan(two, paths.get(one))){
+                System.out.println(" Can surf from " + one + " to " + two);
+                if (input.hasNextLine()){
+                    one = input.nextLine();
+                    if (input.hasNextLine()){
+                        two = input.nextLine();
+                    }
+                }
+            }else{
+                System.out.println("Can't surf from " + one + " to " + two);
+            }
+        }
+    }
+}
