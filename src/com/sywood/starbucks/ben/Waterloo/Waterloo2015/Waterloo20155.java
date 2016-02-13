83f0f87b973835fa6d08805a239f2c8d79ac90a6
BenjaminBLi <StrikerB8@gmail.com>
2016-02-12 22:31:47 -0500

    commited

---
+++ src/com/sywood/starbucks/ben/Waterloo/Waterloo2015/Waterloo20155.java
@@ -0,0 +1,9 @@
+package com.sywood.starbucks.ben.Waterloo.Waterloo2015;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Waterloo20155 {
+
+    private static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+    }
+}
