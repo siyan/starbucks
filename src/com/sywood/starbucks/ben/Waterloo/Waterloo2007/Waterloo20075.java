cea5542c31cc9d9feecd1245d0cd78252bfdd24e
BenjaminBLi <StrikerB8@gmail.com>
2016-02-29 19:04:39 -0500

    commited

--- src/com/sywood/starbucks/ben/Waterloo/Waterloo2007/Waterloo20075.java
+++ src/com/sywood/starbucks/ben/Waterloo/Waterloo2007/Waterloo20075.java
@@ -7,18 +7,14 @@ public class Waterloo20075 {
     static int A = 0;
     static int B = 0;
     static int ways = 0;
-    static ArrayList<Integer> hotels = new ArrayList<>();
-    static void recuVisit(int dist){
+    static void recuVisit(int dist, ArrayList<Integer> hotels, int days){
         if (dist >= 7000){
             ways++;
         }else{
-            for (Integer hotel : hotels) {
-                if (dist + hotel < B & A < dist + hotel) {
-                    recuVisit(hotel);
+            for (int i = 0; i < hotels.size(); i++){
+                if (dist + hotels.get(i) < B*days & A*days < hotels.get(i) + dist){
+                    recuVisit(dist+hotels.get(i), hotels.subList(i, hotels.size()-1), days+1);
                 }
-            }
-            if (hotels.get(dist) == hotels.size()-1){
-                recuVisit(7000);
             }
         }
     }
@@ -27,6 +23,7 @@ public class Waterloo20075 {
         FileReader hotel = new FileReader("data/hotels.in");
         BufferedReader input = new BufferedReader(hotel);
         String[] locs = input.readLine().split(" ");
+        ArrayList<Integer> hotels = new ArrayList<>();
         for (String loc : locs){
             hotels.add(Integer.parseInt(loc));
         }
