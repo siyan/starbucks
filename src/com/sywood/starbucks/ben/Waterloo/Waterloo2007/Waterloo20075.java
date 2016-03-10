52962fbc4ea63636d0fbbabf9f020ba5fcbd1b94
BenjaminBLi <StrikerB8@gmail.com>
2016-03-09 22:00:12 -0500

    commited

--- src/com/sywood/starbucks/ben/Waterloo/Waterloo2007/Waterloo20075.java
+++ src/com/sywood/starbucks/ben/Waterloo/Waterloo2007/Waterloo20075.java
@@ -8,6 +8,7 @@ public class Waterloo20075 {
     static int B = 0;
     static int ways = 0;
     static ArrayList<Integer> hotels = new ArrayList<>();
+    static int[] hotel;
     static void recuVisit(int dist){
         if (dist >= 7000){
             ways++;
@@ -19,10 +20,19 @@ public class Waterloo20075 {
             }
         }
     }
+    static void DPVisit(){
+        for (int i = 0; i < hotel.length; i++){
+            for (int j = i+1; j < hotel.length; j++) {
+                if (A <= hotels.get(j)-hotels.get(i) && B >= hotels.get(j)-hotels.get(i)){
+                    hotel[j] += hotel[i];
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException{
-        FileReader hotel = new FileReader("data/hotels.in");
-        BufferedReader input = new BufferedReader(hotel);
+        FileReader hotelz = new FileReader("data/hotels.in");
+        BufferedReader input = new BufferedReader(hotelz);
         String[] locs = input.readLine().split(" ");
         for (String loc : locs){
             hotels.add(Integer.parseInt(loc));
@@ -30,6 +40,10 @@ public class Waterloo20075 {
         A = Integer.parseInt(input.readLine());
         B = Integer.parseInt(input.readLine());
         int m = Integer.parseInt(input.readLine());
+        hotel = new int[13+m];
+        for (int i = 0; i < 13+m; i++){
+            hotel[i] = 1;
+        }
         for (int i = 0; i < m; i++){
             int x = Integer.parseInt(input.readLine());
             if(!hotels.contains(x)){
@@ -38,6 +52,7 @@ public class Waterloo20075 {
         }
         Collections.sort(hotels);
         recuVisit(0);
-        System.out.println(ways);
+        DPVisit();
+        System.out.println(ways == hotel[13+m-1]);
     }
 }
