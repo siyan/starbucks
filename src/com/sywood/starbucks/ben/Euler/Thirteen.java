20632ee3d8c33d7645c187d22d509ff331ac8da5
BenjaminBLi <StrikerB8@gmail.com>
2016-03-24 23:20:03 -0400

    commited

--- src/com/sywood/starbucks/ben/Euler/Thirteen.java
+++ src/com/sywood/starbucks/ben/Euler/Thirteen.java
@@ -6,7 +6,7 @@ public class Thirteen {
     public static void main(String[] args) throws IOException{
         BufferedReader input = new BufferedReader(new FileReader("C:\\Users\\Striker\\IdeaProjects\\Starbucks\\data\\thirteen.in"));
         BigInteger sum = BigInteger.ZERO;
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 100; i++) {
             BigInteger temp = new BigInteger(input.readLine());
             sum = sum.add(temp);
         }
