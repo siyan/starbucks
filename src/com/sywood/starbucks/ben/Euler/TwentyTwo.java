99fae2d575c5d1c73f24135b8e9cfd4fbfec3ee2
BenjaminBLi <StrikerB8@gmail.com>
2016-04-02 14:04:49 -0400

    commited

--- src/com/sywood/starbucks/ben/Euler/TwentyTwo.java
+++ src/com/sywood/starbucks/ben/Euler/TwentyTwo.java
@@ -1,18 +1,23 @@
 package com.sywood.starbucks.ben.Euler;
 import java.io.*;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class TwentyTwo {
     public static void main(String[] args) throws IOException{
         BufferedReader input = new BufferedReader(new FileReader("C:\\Users\\Striker\\IdeaProjects\\Starbucks\\data\\names.in"));
         String[] names = input.readLine().split(" ");
-        long sum = 0;
+        Collections.sort(Arrays.asList(names));
+        BigInteger sum = BigInteger.ZERO;
         for(int i = 1; i <= names.length; i++){
-            long tot = 0;
+            BigInteger tot = BigInteger.ZERO;
             for (char letter : names[i-1].toCharArray()){
-                tot += (long) letter;
-                tot -= 64;
+                tot = tot.add(BigInteger.valueOf((long)letter - 64));
             }
-            tot = tot*(i);
-            sum += tot;
+            tot = tot.multiply(BigInteger.valueOf(i));
+            sum = sum.add(tot);
         }
+        System.out.println(sum);
     }
 }
