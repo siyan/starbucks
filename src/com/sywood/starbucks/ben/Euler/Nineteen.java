e434ea9caa019d30a783cc148f758a5ce68e6d34
BenjaminBLi <StrikerB8@gmail.com>
2016-04-02 20:51:53 -0400

    commited

--- src/com/sywood/starbucks/ben/Euler/Nineteen.java
+++ src/com/sywood/starbucks/ben/Euler/Nineteen.java
@@ -4,20 +4,16 @@ public class Nineteen {
     public static void main(String[] args){
         int numSundays = 0;
         int[] months = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        int year = 1900;
+        int year = 1901;
         int dayNum = 1;
         for (int month : months) {
             for (int i = 1; i <= month; i++) {
                 dayNum++;
                 if (dayNum == 7) {
                     dayNum = 0;
-                    if (i == 1){
-                        numSundays++;
-                    }
                 }
             }
         }
-        year++;
         while(year < 2001) {
             if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
                 months[1] = 29;
