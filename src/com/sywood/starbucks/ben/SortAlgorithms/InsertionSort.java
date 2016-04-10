ac053d34290b3ed9f6c5eee3e4cbe88c3534e9a7
BenjaminBLi <StrikerB8@gmail.com>
2016-04-10 18:05:41 -0400

    commited

--- src/com/sywood/starbucks/ben/SortAlgorithms/InsertionSort.java
+++ src/com/sywood/starbucks/ben/SortAlgorithms/InsertionSort.java
@@ -4,20 +4,19 @@ public class InsertionSort {
     private static int[] sort(int[] array){
         for (int i = 1; i < array.length; i++) {
             if (array[i] < array[i-1]){
-                for (int j = i; j > 0; j--) {
-                    if (array[j] < array[j-1]){
-                        int temp = array[j];
-                        array[j] = array[j-1];
-                        array[j-1] = temp;
-                    }
+                int temp = array[i];
+                int j;
+                for (j = i-1; j >= 0 && temp < array[j]; j--) {
+                    array[j+1] = array[j];
                 }
+                array[j+1] = temp;
             }
         }
         return array;
     }
 
     public static void main(String[] args){
-        int[] test = new int[]{2, 3, 1, 7};
+        int[] test = new int[]{3, 2, 1, 9, 7, 4,8};
         test = sort(test);
         for (int num : test){
             System.out.print(num + ", ");
