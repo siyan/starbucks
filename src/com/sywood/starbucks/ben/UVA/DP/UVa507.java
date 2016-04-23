dee1426288a1995d57fa827d51571b62e1b46fef
BenjaminBLi <StrikerB8@gmail.com>
2016-04-22 23:00:54 -0400

    commited

---
+++ src/com/sywood/starbucks/ben/UVA/DP/UVa507.java
@@ -0,0 +1,14 @@
+package com.sywood.starbucks.ben.UVA.DP;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class UVa507 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        int b = Integer.parseInt(input.readLine());
+        for (int i = 0; i < b; i++) {
+
+        }
+    }
+}
