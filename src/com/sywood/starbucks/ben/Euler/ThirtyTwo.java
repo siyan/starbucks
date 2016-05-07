7952c058dac413215d6d84ed16123c0a0cc19391
BenjaminBLi <StrikerB8@gmail.com>
2016-05-06 22:22:31 -0400

    commited

--- src/com/sywood/starbucks/ben/Euler/ThirtyTwo.java
+++ src/com/sywood/starbucks/ben/Euler/ThirtyTwo.java
@@ -1,7 +1,37 @@
 package com.sywood.starbucks.ben.Euler;
 
 public class ThirtyTwo {
-    public static void main(String[] args){
+    private static boolean isPandigital(int first, int second, int third){
+        if (third < 1000){
+            return false;
+        }else{
+            int count = 0;
+            while (first > 0){
+                count |= 1 << (first % 10);
+                first /= 10;
+            }
+            while (second > 0){
+                count |= 1 << (second % 10);
+                second /= 10;
+            }
+            while (third > 0){
+                count |= 1 << (third % 10);
+                third /= 10;
+            }
+            return (count>>1 == (1<<9)-1);
+        }
+    }
 
+    public static void main(String[] args){
+        int sum = 0;
+        for (int i = 1; i < 98; i++) {
+            for (int j = 100; j < 987; j++) {
+                int product = i*j;
+                if (isPandigital(i, j, product)){
+                    sum += product;
+                }
+            }
+        }
+        System.out.println(sum);
     }
 }
