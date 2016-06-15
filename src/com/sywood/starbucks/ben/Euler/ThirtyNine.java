7bc19eacf87d1469ec74c5408eee33dc0ddbe1f3
BenjaminBLi <strikerb8@gmail.com>
2016-06-14 22:46:38 -0400

    done euler 36

--- src/com/sywood/starbucks/ben/Euler/ThirtyNine.java
+++ src/com/sywood/starbucks/ben/Euler/ThirtyNine.java
@@ -1,7 +1,26 @@
 package com.sywood.starbucks.ben.Euler;
+import java.util.ArrayList;
 
-/**
- * Created by Ben on 2016-06-14.
- */
 public class ThirtyNine {
+    private static long findNum(){
+        long ret = 0;
+        long resultSolutions = 0;
+        for (long p = 2; p <= 1000; p += 2){
+            int numOfSolutions = 0;
+            for (long a = 2; a < (p/3); a++) {
+                if (p * (p - 2 * a) % (2*(p-a)) == 0){
+                    numOfSolutions++;
+                }
+            }
+            if (numOfSolutions > resultSolutions){
+                resultSolutions = numOfSolutions;
+                ret = p;
+            }
+        }
+        return ret;
+    }
+
+    public static void main(String[] args){
+        System.out.println(findNum());
+    }
 }
