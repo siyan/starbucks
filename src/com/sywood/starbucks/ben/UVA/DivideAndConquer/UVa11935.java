4449747a698ec2e9aabaeee4e2f41d308311ddb9
BenjaminBLi <strikerb8@gmail.com>
2016-06-21 22:05:15 -0400

    Started UVa11935

--- src/com/sywood/starbucks/ben/UVA/DivideAndConquer/UVa11935.java
+++ src/com/sywood/starbucks/ben/UVA/DivideAndConquer/UVa11935.java
@@ -2,8 +2,46 @@ package com.sywood.starbucks.ben.UVA.DivideAndConquer;
 import java.io.*;
 
 public class UVa11935 {
-    private static boolean simulate(double gas){
-        return false;
+    private static boolean can(double gas, String[][] data, int i){
+        double cons = Double.parseDouble(data[0][3])/100;
+        int curr = 0;
+        double max = gas;
+        double leak = 0;
+        for (int j = 0; j <= i; j++){
+            String[] line = data[j];
+            int dist = Integer.parseInt(line[0]);
+            int moved = dist-curr;
+            gas -= moved*cons;
+            if (gas < 0){
+                return false;
+            }
+            switch (line[1]) {
+                case "Leak":
+                    leak++;
+                    break;
+                case "Fuel":
+                    cons = Double.parseDouble(line[3]) / 100;
+                    break;
+                case "Gas":
+                    gas = max;
+                    break;
+                case "Mechanic":
+                    leak = 0;
+                    break;
+            }
+            gas -= leak*moved;
+            curr = dist;
+        }
+        return true;
+    }
+    private static double find(String[][] data, int i){
+        double min = 0.0, max = 10000.0, mid, ans = 0.0;
+        for (int j = 0; j < 100; j++) {
+            mid = (max+min)/2.0;
+            if (can(mid, data, i)){ ans = mid; max = mid;}
+            else{min = mid;}
+        }
+        return ans;
     }
 
     public static void main(String[] args) throws IOException{
@@ -11,10 +49,19 @@ public class UVa11935 {
         String line = input.readLine();
         String[] info;
         while (!line.equals("0 Fuel consumption 0")){
+            String[][] simulationData = new String[50][3];
             info = line.split(" ");
-            double fuel = 0.0;
-
+            int i = 0;
+            while (!info[1].equals("Goal")){
+                simulationData[i] = info;
+                line = input.readLine();
+                info = line.split(" ");
+                i++;
+            }
+            simulationData[i] = info;
+            System.out.printf("%.3f", find(simulationData, i));
             line = input.readLine();
+            System.out.print("\n");
         }
     }
 }
