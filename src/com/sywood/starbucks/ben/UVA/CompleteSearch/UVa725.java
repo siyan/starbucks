396ea4e23d7b66f5bbe2e553a42d66c880a29753
BenjaminBLi <StrikerB8@gmail.com>
2016-06-24 22:47:05 -0400

    Started UVa725

--- src/com/sywood/starbucks/ben/UVA/CompleteSearch/UVa725.java
+++ src/com/sywood/starbucks/ben/UVA/CompleteSearch/UVa725.java
@@ -6,11 +6,15 @@ import java.util.LinkedList;
 import java.util.Arrays;
 
 public class UVa725 {
-
-
     public static void main(String[] args)throws IOException{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(input.readLine());
+        while (N != 0) {
+            for (int fghij = 1234; fghij <= 98765; fghij++) {
+                int temp = 0;
+                int abcde = fghij*N;
 
+            }
+        }
     }
 }
