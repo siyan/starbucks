d52e16c1c4829ab4ef66f0d3dc8d7bda693cae40
BenjaminBLi <strikerb8@gmail.com>
2016-06-26 11:40:39 -0400

    Started UVa11742

---
+++ src/com/sywood/starbucks/ben/UVA/CompleteSearch/UVa11742.java
@@ -0,0 +1,13 @@
+package com.sywood.starbucks.ben.UVA.CompleteSearch;
+import java.io.*;
+public class UVa11742 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String[] stuff = input.readLine().split(" ");
+        int n = Integer.parseInt(stuff[0]);
+        int m = Integer.parseInt(stuff[1]);
+        for (int i = 0; i < m; i++) {
+            int counter = 0;
+        }
+    }
+}
