25b8ad8c7a646622f0685269441663ef9ce2796c
BenjaminBLi <strikerb8@gmail.com>
2016-06-27 21:37:44 -0400

    Finished Uva11172

--- src/com/sywood/starbucks/ben/UVA/CompleteSearch/UVa11742.java
+++ src/com/sywood/starbucks/ben/UVA/CompleteSearch/UVa11742.java
@@ -3,8 +3,8 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class UVa11742 {
+    private static int counter = 0;
     private static int permute(int[] arr, int current, ArrayList<String[]> data){
-        int counter = 0;
         for (int i = current; i < arr.length; i++){
             int temp = arr[current];
             arr[current] = arr[i];
@@ -13,13 +13,8 @@ public class UVa11742 {
             arr[i] = arr[current];
             arr[current] = temp;
         }
-        if (current == arr.length-1){
-            System.out.print("[");
-            for (int i = 0; i < arr.length; i++) {
-                System.out.print(arr[i] + ", ");
-            }
-            System.out.print("]\n");
-            counter += test(arr, data) ? 1 : 0;
+        if (current == arr.length-1 && test(arr, data)){
+            counter++;
         }
         return counter;
     }
@@ -27,14 +22,24 @@ public class UVa11742 {
     private static boolean can(int[] arr, String[] data) {
         int first = Integer.parseInt(data[0]);
         int second = Integer.parseInt(data[1]);
+        int req = Integer.parseInt(data[2]);
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == first) {
                 first = i;
-            } else if (arr[i] == second) {
+            }if (arr[i] == second) {
                 second = i;
             }
         }
-        return first - second == Integer.parseInt(data[2]);
+        if (req > 0) {
+            if (Math.abs(first - second) > req){
+                return false;
+            }
+        }else{
+            if (Math.abs(first - second) < -req){
+                return false;
+            }
+        }
+        return true;
     }
 
     private static boolean test(int[] arr, ArrayList<String[]> data) {
@@ -56,6 +61,7 @@ public class UVa11742 {
         int n = Integer.parseInt(stuff[0]);
         int m = Integer.parseInt(stuff[1]);
         while (n != m || m != 0 || n != 0) {
+            counter = 0;
             int[] seats = new int[n];
             for (int i = 0; i < n; i++) {
                 seats[i] = i;
