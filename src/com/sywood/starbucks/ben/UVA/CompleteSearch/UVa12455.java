6a2f8f4b4f86aaf14681d7dce6d6d1a20b73b434
BenjaminBLi <strikerb8@gmail.com>
2016-06-28 15:40:54 -0400

    Finished UVa12455

--- src/com/sywood/starbucks/ben/UVA/CompleteSearch/UVa12455.java
+++ src/com/sywood/starbucks/ben/UVA/CompleteSearch/UVa12455.java
@@ -2,18 +2,34 @@ package com.sywood.starbucks.ben.UVA.CompleteSearch;
 import java.io.*;
 
 public class UVa12455 {
-
-
     public static void main(String[] args) throws IOException{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        int iters = Integer.parseInt(input.readLine());
-        for (int i = 0; i < iters; i++) {
+        int t = Integer.parseInt(input.readLine());
+        for (int a = 0; a < t; a++) {
+            int target = Integer.parseInt(input.readLine());
             int n = Integer.parseInt(input.readLine());
-            int p = Integer.parseInt(input.readLine());
-            int[] lengths = new int[p];
-            int permutationSize = 1;
-            while (permutationSize <= n){
-                
+            int[] bars = new int[n];
+            int each = 0;
+            boolean found = false;
+            for (String bar : input.readLine().split(" ")){
+                bars[each] = Integer.parseInt(bar);
+                each++;
+            }
+            for (int i = 0; i < 1<<n; i++){
+                int sum = 0;
+                for (int j = 0; j < n; j++) {
+                    if ((i>>j)%2 == 1){
+                        sum += bars[j];
+                    }
+                }
+                if (sum == target){
+                    System.out.println("YES");
+                    found = true;
+                    break;
+                }
+            }
+            if (!found){
+                System.out.println("NO");
             }
         }
     }
--- src/com/sywood/starbucks/ben/UnderstandingThings/BitmaskPermutations.java
+++ src/com/sywood/starbucks/ben/UnderstandingThings/BitmaskPermutations.java
@@ -2,9 +2,15 @@ package com.sywood.starbucks.ben.UnderstandingThings;
 
 public class BitmaskPermutations {
     public static void main(String[] args){
-        int max = 4; // number of items.
-        for (int i = 1<<(max-1); i < 1<<max; i++) {
+        /*
+        int max = 10; // number of items.
+        for (int i = 0; i < 1<<max; i++) {
             System.out.printf("%5s\n", Integer.toBinaryString(i));
+        }*/
+        int i = (1<<2)-1;
+        System.out.println(i);
+        for (int j = 0; j < 2; j++) {
+            System.out.println(Integer.toBinaryString(i>>j%2) + " " + ((i>>j)%2 == 1));
         }
     }
 }
