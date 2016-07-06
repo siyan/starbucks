e2025bd0756b3fabc3b0c4a285c858df334ea059
BenjaminBLi <strikerb8@gmail.com>
2016-07-06 00:20:26 -0400

    Finished reverse

---
+++ src/com/sywood/starbucks/ben/SIUCFcompetitions/FirstPreCamp/party.java
@@ -0,0 +1,35 @@
+//package com.sywood.starbucks.ben.SIUCFcompetitions.FirstPreCamp;
+import java.io.*;
+import java.util.Arrays;
+
+public class party {
+    public static void main(String[] args)throws IOException{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String line = input.readLine();
+        while (!line.equals("0")){
+            int n = Integer.parseInt(line);
+            String[] names = new String[n];
+            for (int i = 0; i < n; i++) {
+                names[i] = input.readLine();
+            }
+            Arrays.sort(names);
+            int center = (n-1)/2;
+            String first = names[center];
+            String second = names[center+1];
+            String middle = "";
+            for (int i = 0; i < Math.min(first.length(), second.length()); i++) {
+                char f = first.charAt(i);
+                char s = second.charAt(i);
+                int inbetween = (f+s)/2;
+                if (inbetween != f){
+                    inbetween = (char)(f+1);
+                    middle += (char)inbetween;
+                    break;
+                }
+                middle += (char)inbetween;
+            }
+            System.out.println(middle);
+            line = input.readLine();
+        }
+    }
+}
---
+++ src/com/sywood/starbucks/ben/SIUCFcompetitions/FirstPreCamp/reverse.java
@@ -0,0 +1,57 @@
+//package com.sywood.starbucks.ben.SIUCFcompetitions.FirstPreCamp;
+import java.io.*;
+
+public class reverse {
+    public static void main(String[] args) throws IOException{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String line = input.readLine();
+        while (!line.equals("0")){
+            int n = Integer.parseInt(line);
+            char[][] board = new char[n][n];
+            for (int i = 0; i < n; i++) {
+                board[i] = input.readLine().toCharArray();
+            }
+            for (char[] row : board){
+                int count = 0;
+                String toPrint = "";
+                for (char col : row){
+                    if (col == 'X'){
+                        count++;
+                    }else if (col == '.' && count > 0){
+                        toPrint += count + " ";
+                        count = 0;
+                    }
+                }
+                if (count > 0){
+                    toPrint += count;
+                }
+                if (!toPrint.equals("")) {
+                    System.out.println(toPrint.trim());
+                }else{
+                    System.out.println(0);
+                }
+            }
+            for (int col = 0; col < n; col++){
+                String toPrint = "";
+                int count = 0;
+                for(int row = 0; row < n; row++){
+                    if (board[row][col] == 'X'){
+                        count++;
+                    }else if (board[row][col] == '.' && count > 0){
+                        toPrint += count + " ";
+                        count = 0;
+                    }
+                }
+                if (count > 0){
+                    toPrint += count;
+                }
+                if (!toPrint.equals("")) {
+                    System.out.println(toPrint.trim());
+                }else{
+                    System.out.println(0);
+                }
+            }
+            line = input.readLine();
+        }
+    }
+}
