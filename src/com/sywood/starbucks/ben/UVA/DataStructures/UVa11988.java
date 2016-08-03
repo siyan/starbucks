4fad61fc8353e64334d4ea78cc4cc953d1b815f6
BenjaminBLi <strikerb8@gmail.com>
2016-08-03 17:28:36 -0400

    Finished UVa11988

---
+++ src/com/sywood/starbucks/ben/UVA/DataStructures/UVa11988.java
@@ -0,0 +1,59 @@
+package com.sywood.starbucks.ben.UVA.DataStructures;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayDeque;
+
+/**
+ * UVa's only sole linkedList question.
+ */
+public class UVa11988 {
+    public static void main(String[] args)throws Exception{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter printer = new PrintWriter(System.out);
+        ArrayDeque<String> letters;
+        boolean begin;
+        String line = input.readLine();
+        String temp;
+        while(line != null && !line.equals("")){
+            letters = new ArrayDeque<>();
+            char[] stuff = line.toCharArray();
+            temp = "";
+            begin = false;
+            for(char thing : stuff){
+                if(thing == '['){
+                    if (begin){
+                        letters.addFirst(temp);
+                    }else{
+                        letters.addLast(temp);
+                    }
+                    temp = "";
+                    begin = true;
+                }else if (thing == ']'){
+                    if (begin){
+                        letters.addFirst(temp);
+                    }else
+                        letters.addLast(temp);
+                    temp = "";
+                    begin = false;
+                }else
+                    temp += thing;
+            }
+
+            if(!temp.equals("")){
+                if(begin){
+                    letters.addFirst(temp);
+                }else
+                    letters.addLast(temp);
+            }
+
+            for(String lett : letters){
+                printer.print(lett);
+            }
+            printer.print("\n");
+            line = input.readLine();
+        }
+        printer.close();
+    }
+}
