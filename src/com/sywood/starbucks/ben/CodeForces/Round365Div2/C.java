7db6e2d34dc99268a04b05774a525c9fee7b1abc
BenjaminBLi <strikerb8@gmail.com>
2016-08-07 23:32:25 -0400

    Fixing codeforces contest 366 div 2 problem c

--- src/com/sywood/starbucks/ben/CodeForces/Round365Div2/C.java
+++ src/com/sywood/starbucks/ben/CodeForces/Round365Div2/C.java
@@ -2,13 +2,17 @@ package com.sywood.starbucks.ben.CodeForces.Round365Div2;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 /**
- * Created by Ben on 2016-08-04.
+ * did this after contest
  */
 public class C {
     public static void main(String[] args)throws Exception{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter printer = new PrintWriter(System.out);
+        StringTokenizer st= new StringTokenizer(input.readLine());
 
     }
 }
