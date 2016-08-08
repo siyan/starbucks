05d154c6900b3552890269e718d3f1ed30c4b6ae
BenjaminBLi <strikerb8@gmail.com>
2016-08-08 16:10:22 -0400

    Finished UVa 732

--- src/com/sywood/starbucks/ben/UVA/DataStructures/UVa732.java
+++ src/com/sywood/starbucks/ben/UVA/DataStructures/UVa732.java
@@ -3,41 +3,58 @@ package com.sywood.starbucks.ben.UVA.DataStructures;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.Stack;
+import java.util.Arrays;
 
 /**
  * Second underlined stack question
  */
 public class UVa732 {
-    private static String line;
-    private static PrintWriter printer = new PrintWriter(System.out);
-    private static void recurse(String current, Stack<Character> stack, String target, String process){
-        if (stack.isEmpty() && target.equals("")){
-            printer.println(process);
-        }else if (!current.equals("")){
-            stack.push(current.charAt(0));
-            recurse(current.substring(1), stack, target, process+ "i ");
-        }else if (!stack.isEmpty()){
-            char t = stack.pop();
-            if (t == target.charAt(0)){
-                recurse(current, stack, target.substring(1), process + "o ");
+    static boolean isGood(String a, String b) {
+        char[] aa = a.toCharArray();
+        char[] bb = b.toCharArray();
+        Arrays.sort(aa);
+        Arrays.sort(bb);
+        for (int i = 0; i < aa.length; i++) {
+            if (aa[i] != bb[i]) {
+                return false;
             }
         }
-
+        return true;
     }
 
-    public static void main(String[] args)throws Exception{
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        Stack<Character> stack;
-        line = input.readLine();
-        String target;
-        while(line != null && !line.equals("")){
-            target = input.readLine();
-            stack = new Stack<>();
-            stack.push(line.charAt(0));
-            printer.println("[");
-            recurse(line.substring(1), stack, target, "i ");
-            printer.println("]");
+    static void dfs(StringBuilder input, String target, StringBuilder cur, StringBuilder stack, StringBuilder path, int n) {
+        if (cur.toString().equals(target)) {
+            System.out.println(path.toString().trim());
+        } else {
+            if (input.length() > 0) {
+                char c = input.charAt(0);
+                String x = path.toString();
+                dfs(input.deleteCharAt(0), target, cur, stack.insert(0, c), path.append("i "), n + 1);
+                input.insert(0, c);
+                stack.deleteCharAt(0);
+                path = new StringBuilder(x);
+            }
+            if (stack.length() > 0 && stack.charAt(0) == target.charAt(cur.length())) {
+                char c = stack.charAt(0);
+                String x = path.toString();
+                dfs(input, target, cur.append(c), stack.deleteCharAt(0), path.append("o "), n + 1);
+                cur.deleteCharAt(cur.length() - 1);
+                stack.insert(0, c);
+                path = new StringBuilder(x);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String source;
+        while ((source = br.readLine()) != null) {
+            String target = br.readLine();
+            System.out.println("[");
+            if (source.length() == target.length() && isGood(source, target)) {
+                dfs(new StringBuilder(source), target, new StringBuilder(), new StringBuilder(), new StringBuilder(), 0);
+            }
+            System.out.println("]");
         }
     }
 }
