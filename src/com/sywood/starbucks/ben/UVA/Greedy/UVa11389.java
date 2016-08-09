4a8124e9a141be57d510e62e8116fc1c2fee90bf
BenjaminBLi <strikerb8@gmail.com>
2016-08-09 01:52:42 -0400

    Finished UVa11264

--- src/com/sywood/starbucks/ben/UVA/Greedy/UVa11389.java
+++ src/com/sywood/starbucks/ben/UVA/Greedy/UVa11389.java
@@ -1,17 +1,18 @@
-package com.sywood.starbucks.ben.UVA.DataStructures;
+package com.sywood.starbucks.ben.UVA.Greedy;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.Stack;
+import java.util.StringTokenizer;
 
 /**
- * First queue question. ->>> last question in DS, switched to DP
+ * second underlined greedy problem
  */
-public class UVa10172 {
+public class UVa11389 {
     public static void main(String[] args)throws Exception{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter printer = new PrintWriter(System.out);
+        StringTokenizer st;
 
     }
 }
