2c1f4824451db2e984208d30c60e8d810af477d4
BenjaminBLi <strikerb8@gmail.com>
2016-08-23 15:45:01 -0400

    given test case to bookshelf

--- src/com/sywood/starbucks/ben/CodeForces/EducationalRound16/C.java
+++ src/com/sywood/starbucks/ben/CodeForces/EducationalRound16/C.java
@@ -2,22 +2,26 @@ package com.sywood.starbucks.ben.CodeForces.EducationalRound16;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
-/**
- * Created by Ben on 2016-08-22.
- */
 public class C {
     public static void main(String[] args)throws Exception{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(input.readLine());
         int[][] matrix = new int[n][n];
-        boolean[] take = new boolean[n*n];
-        if (n == 1){
-            System.out.println(1);
-        }else{
-            if (n%2 == 0){
-
+        int r = 0, c = n/2;
+        for(int i = 1; i <= n*n; i++) {
+            System.out.println(r + ", " + c);
+            if (matrix[r][c] != 0) {
+                r = ((r+1)%n+1)%n;
+                c = (c-1+n)%n;
             }
+            matrix[r][c] = i;
+            r = (r-1+n)%n;
+            c = (c+1)%n;
+            for(int[] row : matrix)
+                System.out.println(Arrays.toString(row));
+            System.out.println();
         }
     }
 }
