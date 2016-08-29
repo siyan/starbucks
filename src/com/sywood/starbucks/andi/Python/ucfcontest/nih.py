6506de427c11ad01b3cdfa07bffd184c07007867
Andi Gu <andi.gu.ca@gmail.com>
2016-08-29 00:18:28 -0400

    shortened nih solution for fun

--- src/com/sywood/starbucks/andi/Python/ucfcontest/nih.py
+++ src/com/sywood/starbucks/andi/Python/ucfcontest/nih.py
@@ -1,17 +1,10 @@
 def brute_force(values, money):
-    best = 0
-    for i in range(len(values)):
-        for j in values[i]:
-            if j <= money:
-                best = max(values[i][j] + brute_force(values[:i] + values[i + 1:], money - j), best)
-    return best
+    return max([values[i][j][1] + brute_force(values[:i] + values[i + 1:], money - values[i][j][0]) for i in range(len(values)) for j in range(4) if values[i][j][0] <= money] + [0])
 
 for x in range(int(input())):
     n, money = [int(i) for i in input().split()]
     values = []
     for i in range(n):
         data = [int(i) for i in input().split()]
-        values.append({})
-        for j in range(4):
-            values[-1][data[j * 2]] = data[j * 2 + 1]
+        values.append([[data[j * 2], data[j * 2 + 1]] for j in range(4)])
     print("Budget #" + str(x + 1) + ": Maximum number of " + str(brute_force(values, money)) + " lives saved.")
