cc6b280fea2bb7aa39f58f8b562143f6d69491da
BenjaminBLi <strikerb8@gmail.com>
2016-08-31 22:24:35 -0400

    Finished UVa11517

--- src/com/sywood/starbucks/ben/UVA/DP/UVa11517.java
+++ src/com/sywood/starbucks/ben/UVA/DP/UVa11517.java
@@ -5,35 +5,29 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 /**
- * Created by Ben on 2016-08-31.
+ * Variant of coin change, deals with min change greater than or equal to the target value
  */
 public class UVa11517 {
     private static int price, target;
-    private static int[] coins = new int[]{1, 5, 10, 25, 50};
+    private static int[] coins;
 
     private static long[] coinChange(){
-        long[][] memo = new long[coins.length+1][target+1];
-        for (int i = 0; i < coins.length+1; i++)
-            memo[i][0] = 1; //if the value is 0, there's one way to make 0 cents
-        Arrays.fill(memo[0], 0);
+        long memo[] = new long[30001];
 
-        for(int i = 1; i < coins.length+1; i++) {
-            for(int j = 1; j < target+1; j++){
-                if (coins[i-1] <= j)
-                    memo[i][j] = memo[i-1][j] + memo[i][j - coins[i-1]];
-                else
-                    memo[i][j] = memo[i-1][j];
+        Arrays.fill(memo, Integer.MAX_VALUE);
+        memo[0] = 0;
+
+        for(int coin : coins){
+            for(int value = 30001-coin-1; value >= 0; value--){
+                if (memo[value] < Integer.MAX_VALUE)
+                    memo[value + coin] = Math.min(memo[value+coin], memo[value]+1);
             }
         }
-        if (memo[coins.length][price] > 0){
-            return new long[]{price, memo[coins.length][target]};
-        }
-        for (int i = target+1; i <= target; i++) {
-            if (memo[coins.length][i] > 0){
-                return new long[]{i, memo[coins.length][i]};
+        for(int value = price; value <= 30000; value++){
+            if (memo[value] < Integer.MAX_VALUE){
+                return new long[]{value, memo[value]};
             }
         }
-
         return new long[]{-1, -1};
     }
 
@@ -48,8 +42,8 @@ public class UVa11517 {
                 coins[i] = Integer.parseInt(input.readLine());
             }
             target = price + coins[n-1];
-            long[] results = coinChange();
-            System.out.println(results[0] + " " + results[1]);
+            long[] ret = coinChange();
+            System.out.println(ret[0] + " " + ret[1]);
         }
     }
 }
