2d7a5af8e4811eb3bc766c36bc90c00ad6cc4ed0
BenjaminBLi <strikerb8@gmail.com>
2016-10-02 23:07:21 -0400

    started ABA12c from SPOJ and noi04p1 from WCIPEG

--- src/com/sywood/starbucks/ben/SPOJ/ABA12C.java
+++ src/com/sywood/starbucks/ben/SPOJ/ABA12C.java
@@ -9,6 +9,25 @@ import java.util.StringTokenizer;
  * Created by Ben on 2016-10-02.
  */
 public class ABA12C {
+    private static int[] weights;
+    private static int[] values;
+    private static int money, n;
+    private static int knapsack() {
+        int[][] memo = new int[n+1][money+1];
+
+        for(int i = 0; i <= n; i++){
+            for(int weight = 0; weight <= money; weight++){
+                if(i == 0 || weight == 0)
+                    memo[i][weight] = 0;
+                else if (weights[i-1] <= weight)
+                    memo[i][weight] = Math.max(values[i-1] + memo[i-1][weight-weights[i-1]], memo[i-1][weight]);
+                else
+                    memo[i][weight] = memo[i-1][weight];
+            }
+        }
+
+        return memo[n][money];
+    }
     public static void main(String[] args)throws Exception{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         int C = Integer.parseInt(input.readLine());
---
+++ src/com/sywood/starbucks/ben/WCIPEG/noi04p1.java
@@ -0,0 +1,16 @@
+package com.sywood.starbucks.ben.WCIPEG;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class noi04p1 {
+    public static void main(String[] args)throws Exception{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(input.readLine());
+        long delta = 0;
+        int n = Integer.parseInt(st.nextToken());
+        int min = Integer.parseInt(st.nextToken());
+
+    }
+}
