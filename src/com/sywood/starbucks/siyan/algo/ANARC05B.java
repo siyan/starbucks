0396b4606549a1fa648799d06573ab3a46773dfe
siyan <siyan@macbookair>
2016-10-26 15:59:09 -0400

    merge sort

---
+++ src/com/sywood/starbucks/siyan/algo/ANARC05B.java
@@ -0,0 +1,20 @@
+package com.sywood.starbucks.siyan.algo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by siyan on 2016-10-26.
+ */
+public class ANARC05B {
+    public static void main(String[] args ) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String first = input.readLine();
+
+        int[][] array;
+        while(!first.equals(0)){
+
+        }
+    }
+}
