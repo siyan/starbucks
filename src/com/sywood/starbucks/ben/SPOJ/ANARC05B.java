3954d790dcea00fc9ca3a42d1cfa70f9fb7fbaa1
BenjaminBLi <strikerb8@gmail.com>
2016-10-28 20:23:41 -0400

    Finished ANARC05B

--- src/com/sywood/starbucks/ben/SPOJ/ANARC05B.java
+++ src/com/sywood/starbucks/ben/SPOJ/ANARC05B.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.Arrays;
 
 /**
  TODO learn binary search better and finish this problem
@@ -13,31 +12,40 @@ import java.util.Arrays;
 public class ANARC05B {
     private static int firstLen, secLen;
     private static int[] first = new int[10001], sec = new int[10001];
-    private static int[] memo; //stores best sum up to and including the idx.
 
-    //first write recursive solution
-    private static int solve(int fIdx, int sIdx, int pos, int sum){
-        //System.out.println(fIdx + ", " + sIdx + ", " + pos + ", " + sum);
-        if(fIdx >= firstLen || sIdx >= secLen)
-            return sum;
-        else {
-            int idx = pos == 0 ? fIdx : sIdx;
-
-            int best = Integer.MIN_VALUE;
-            if(first[fIdx] == sec[sIdx]){
-                best = solve(fIdx+1, sIdx+1, (pos+1)&1, sum + first[fIdx]);
-            }
-
-            sum += pos == 0 ? first[fIdx] : sec[sIdx];
-
-            best = best > solve(fIdx+1, sIdx, pos, sum) ? best : solve(fIdx+1, sIdx, pos, sum);
-            best = best > solve(fIdx+1, sIdx+1, pos, sum) ? best : solve(fIdx+1, sIdx+1, pos, sum);
-            best = best > solve(fIdx, sIdx+1, pos, sum) ? best : solve(fIdx, sIdx+1, pos, sum);
-
-            return best;
-        }
+    private static int max(int a, int b){
+        return a > b ? a : b;
     }
 
+    private static int solve(){
+        int best = 0;
+        int tSum = 0, bSum = 0;
+
+        for(int tI = 0, bI = 0; tI < firstLen || bI < secLen;){
+            if(tI < firstLen && bI < secLen) {
+
+                if (sec[bI] < first[tI]) {
+                    bSum += sec[bI];
+                    bI++;
+                } else if (first[tI] < sec[bI]){
+                    tSum += first[tI];
+                    tI++;
+                }else {
+                    best += first[tI];
+                    best += max(tSum, bSum);
+                    tI++;
+                    tSum = 0;
+                    bI++;
+                    bSum = 0;
+                }
+            }else if (tI < firstLen){
+                tSum += first[tI++];
+            }else{
+                bSum += sec[bI++];
+            }
+        }
+        return best + max(tSum, bSum);
+    }
 
     public static void main(String[] args)throws Exception{
         FastScanner input = new FastScanner(System.in);
@@ -50,11 +58,8 @@ public class ANARC05B {
             for (int i = 0; i < secLen; i++) {
                 sec[i] = input.nextInt();
             }
-            memo = new int[firstLen > secLen ? firstLen : secLen];
-            Arrays.fill(memo, Integer.MIN_VALUE);
-            memo[0] = first[0] > sec[0] ? first[0] : sec[0];
-            System.out.println(solve(1, 1, 0, memo[0]));
-            System.out.println(Arrays.toString(memo));
+
+            System.out.println(solve());
             firstLen = input.nextInt();
         }
     }
--- src/com/sywood/starbucks/ben/UVA/CompleteSearch/TrailerUVa10976.java
+++ src/com/sywood/starbucks/ben/UVA/CompleteSearch/TrailerUVa10976.java
@@ -1,4 +1,5 @@
 package com.sywood.starbucks.ben.UVA.CompleteSearch;
+
 import java.io.*;
 import java.util.ArrayList;
 public class TrailerUVa10976 {
@@ -22,17 +23,17 @@ public class TrailerUVa10976 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter printer = new PrintWriter(System.out);
         String line = input.readLine();
-        String nextLine = input.readLine();
-        while (nextLine != null && !nextLine.equals("")){
+        while (line != null && !line.equals("")) {
             int num = Integer.parseInt(line);
             ArrayList<Integer> read = calculate(num);
-            System.out.println(read.get(0));
-            for (int i = 1; i < read.size(); i+= 2) {
-                System.out.printf("1/%d = 1/%d + 1/%d\n", num, read.get(i), read.get(i+1));
+            printer.println(read.get(0));
+            for (int i = 1; i < read.size(); i += 2) {
+                printer.printf("1/%d = 1/%d + 1/%d\n", num, read.get(i), read.get(i + 1));
             }
-            line = nextLine;
-            nextLine = input.readLine();
+            line = input.readLine();
         }
+        printer.close();
     }
 }
