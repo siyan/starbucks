386ae458444f14c0f7c7fd9889fddf1349f251c0
BenjaminBLi <strikerb8@gmail.com>
2016-11-10 23:21:56 -0500

    Finished challenge question number 2

---
+++ src/com/sywood/starbucks/ben/ICSChallengeQuestions/Q2.java
@@ -0,0 +1,19 @@
+package com.sywood.starbucks.ben.ICSChallengeQuestions;
+
+/**
+ * Created by Ben on 2016-11-10.
+ */
+public class Q2 {
+    public static int method(int angle, int bounce){
+        if(bounce == 0){
+            return 360-angle;
+        }else if (bounce == 1){
+             return 270+angle;
+        }else if (bounce == 2){
+            return 90+angle;
+        }else if (bounce == 3){
+            return 180+angle;
+        }
+        return -1;
+    }
+}
