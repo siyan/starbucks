1c986a0888009ad7d4dc3ee6aa5d623d73e55009
BenjaminBLi <strikerb8@gmail.com>
2016-11-13 21:58:07 -0500

    Should be done ccc00s3

--- src/com/sywood/starbucks/ben/Waterloo/Waterloo2000/ccc00s3.java
+++ src/com/sywood/starbucks/ben/Waterloo/Waterloo2000/ccc00s3.java
@@ -1,14 +1,15 @@
 package com.sywood.starbucks.ben.Waterloo.Waterloo2000;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.*;
 
 public class ccc00s3 {
-    static boolean[][] matrix = new boolean[101][101];
+    static boolean[][] matrix = new boolean[2500][2500];
     static int max;
     private static boolean bfs(int start, int end){
         ArrayDeque<Integer> q = new ArrayDeque<>();
-        boolean[] visited = new boolean[101];
+        boolean[] visited = new boolean[2500];
         visited[start] = true;
         for (int i = 0; i < max; i++) {
             if(matrix[start][i]) q.addLast(i);
@@ -25,7 +26,8 @@ public class ccc00s3 {
 
     public static void main(String[] args)throws Exception{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(input.readLine());
+        PrintWriter printer = new PrintWriter(System.out);
+        int n = Integer.parseInt(input.readLine().trim());
 
         HashMap<String, Integer> urlToIdx = new HashMap<>();
         int count = 0;
@@ -34,28 +36,30 @@ public class ccc00s3 {
             curr = input.readLine();
             urlToIdx.put(curr, count++);
             for(String line = input.readLine(); !line.equals("</HTML>"); line = input.readLine()){
-                if(line.contains("<A HREF=")){
-                    String[] arr = line.split("<A HREF=");
-                    for(String link : arr){
-                        if(link.contains("\">")) {
-                            String lnk = link.substring(1, link.indexOf("\">"));
-                            if (!urlToIdx.containsKey(lnk))
-                                urlToIdx.put(lnk, count++);
-                            System.out.println("Link from " + curr + " to " + lnk);
-                            matrix[urlToIdx.get(curr)][urlToIdx.get(lnk)] = true;
-                        }
-                    }
+                int startPos = line.indexOf("HREF=");
+                while( startPos > -1 ) {
+                    int endPos = line.indexOf("\"", startPos+6);
+                    String linkUrl = line.substring( startPos+6, endPos);
+                    if(!urlToIdx.containsKey(linkUrl))
+                        urlToIdx.put( linkUrl, count++ );
+
+                    printer.println("Link from " + curr + " to " + linkUrl);
+                    matrix[urlToIdx.get(curr)][urlToIdx.get(linkUrl)] = true;
+                    startPos = line.indexOf("HREF=", endPos);
                 }
             }
         }
+
         max = count;
         //all links have been processed
         String l1 = input.readLine();
         String l2;
-        for(; !l1.trim().equals("The End"); l1 = input.readLine()){
+        for(; !l1.trim().equals("The End"); ){
             l2 = input.readLine();
-            if(bfs(urlToIdx.get(l1), urlToIdx.get(l2))) System.out.println("Can surf from " + l1 + " to " + l2 + ".");
-            else System.out.println("Can't surf from " + l1 + " to " + l2 + ".");
+            if(bfs(urlToIdx.get(l1), urlToIdx.get(l2))) printer.println("Can surf from " + l1 + " to " + l2 + ".");
+            else printer.println("Can't surf from " + l1 + " to " + l2 + ".");
+            l1 = input.readLine();
         }
+        printer.close();
     }
 }
