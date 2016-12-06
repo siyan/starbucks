b80a5bb38f2e7ea0bfe0d5cd1b705a782872b197
BenjaminBLi <li.benjamin.b@gmail.com>
2016-12-05 21:35:38 -0500

    Tried to improve Zombie Apocalypse

--- src/com/sywood/starbucks/ben/WCIPEG/ccc16s2p5.java
+++ src/com/sywood/starbucks/ben/WCIPEG/ccc16s2p5.java
@@ -3,6 +3,7 @@ package com.sywood.starbucks.ben.WCIPEG;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.TreeMap;
 
 /**
@@ -18,20 +19,54 @@ import java.util.TreeMap;
  restructure to use window
  */
 public class ccc16s2p5 {
-    static int min(int a, int b){
+    private static int min(int a, int b){
         return a < b ? a : b;
     }
-    static int max(int a, int b){
+    private static int max(int a, int b){
         return a > b ? a : b;
+    }
+    private static int K, N, M;
+    private static Zombie[] zombies;
+    private static long area(TreeMap<Integer, Integer> compx, TreeMap<Integer, Integer> compy,
+                   ArrayList<Integer> xs, ArrayList<Integer> ys, int Q){
+        int diff[][] = new int[4002][4002];
+
+        for (int i = 0; i < K; i++) {
+            int r1 = max(compx.get(zombies[i].r) - Q-1, 0), r2 = min(compx.get(zombies[i].r) + Q, N); //subtract one to include a point
+            int c1 = max(compy.get(zombies[i].c) - Q-1, 0), c2 = min(compy.get(zombies[i].c) + Q, M); //same thing here
+            diff[r1][c1]++;
+            diff[r1][c2]--;
+            diff[r2][c1]--;
+            diff[r2][c2]++;
+        }
+
+        long ans = 0;
+        for (int i = 1; i <= xs.size(); i++) {
+            for (int j = 1; j <= ys.size(); j++) {
+                diff[i][j] += diff[i - 1][j] + diff[i][j - 1] - diff[i - 1][j - 1];
+                if (diff[i][j] > 0){
+                    ans += ((long) xs.get(i) - xs.get(i - 1)) * ((long) ys.get(j) - ys.get(j - 1));
+                }
+            }
+        }
+        return ans;
+    }
+    static class Zombie{
+        int r, c;
+        Zombie(int r, int c){
+            this.r = r;
+            this.c = c;
+        }
     }
 
     public static void main(String[] args)throws Exception{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         String[] data = input.readLine().split(" ");
-        int N = Integer.parseInt(data[0]);
-        int M = Integer.parseInt(data[1]);
-        int K = Integer.parseInt(input.readLine());
+        N = Integer.parseInt(data[0]);
+        M = Integer.parseInt(data[1]);
+        K = Integer.parseInt(input.readLine());
 
+        zombies = new Zombie[K];
         TreeMap<Integer, Integer> compx = new TreeMap<>();
         TreeMap<Integer, Integer> compy = new TreeMap<>();
 
@@ -39,6 +74,8 @@ public class ccc16s2p5 {
             data = input.readLine().split(" ");
             compx.put(Integer.parseInt(data[0]), 0);
             compy.put(Integer.parseInt(data[1]), 0);
+            zombies[i] = new Zombie(Integer.parseInt(data[0]), Integer.parseInt(data[1]));
+            //find some way to input the coordinates and not the actual points in the data.
         }
 
         int cntx = 1, cnty = 1;
@@ -53,39 +90,11 @@ public class ccc16s2p5 {
             compy.put(key, cnty++);
             ys.add(key);
         }
-
         //compressed the zombies to a point. Need to scan for each point now?
         int Q = Integer.parseInt(input.readLine());
         if(Q == 0) System.out.println(K);
         else {
-            int diff[][] = new int[4002][4002];
-            //generate the perimeter of each zombie, looking for points that are smaller
-            for (int i = 0; i < K; i++) {
-                int r1 = max(xs.get(i) - Q-1, 0), r2 = min(xs.get(i) + Q, N); //subtract one to include a point
-                int c1 = max(ys.get(i) - Q-1, 0), c2 = min(ys.get(i) + Q, M); //same thing here
-                diff[r1][c1] += 1;
-                diff[r1][c2] -= 1;
-                diff[r2][c1] -= 1;
-                diff[r2][c2] += 1;
-            }
-
-            long ans = 0;
-            for (int i = 1; i < xs.size(); i++) {
-                for (int j = 1; j < ys.size(); j++) {
-                    diff[i][j] += diff[i - 1][j] + diff[i][j - 1] - diff[i - 1][j - 1];
-                    if (diff[i][j] > 0) ans += ((long) xs.get(i) - xs.get(i - 1)) * ((long) ys.get(j) - ys.get(j - 1));
-                }
-            }
-            long ans2 = 0;
-            diff = new int[4002][4002];
-            for (int i = 1; i < xs.size(); i++) {
-                for (int j = 1; j < ys.size(); j++) {
-                    diff[i][j] += diff[i - 1][j] + diff[i][j - 1] - diff[i - 1][j - 1];
-                    if (diff[i][j] > 0)
-                        ans += ((long) xs.get(i) - xs.get(i - 1)) * ((long) ys.get(j) - ys.get(j - 1));
-                }
-            }
-            System.out.println(ans + ", " + ans2);
+            System.out.println(area(compx, compy, xs, ys, Q) + ", " + area(compx, compy, xs, ys, Q-1));
         }
     }
 }
