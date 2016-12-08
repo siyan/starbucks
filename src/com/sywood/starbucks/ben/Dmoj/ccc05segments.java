8e6de9aea3069aa58c6d100676da34613df1a2e8
BenjaminBLi <strikerb8@gmail.com>
2016-12-07 23:12:11 -0500

    FInished ccc16s2p5

---
+++ src/com/sywood/starbucks/ben/Dmoj/ccc05segments.java
@@ -0,0 +1,15 @@
+package com.sywood.starbucks.ben.Dmoj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * it had a dp tag so I did it
+ */
+public class ccc05segments {
+    public static void main(String[] args) throws Exception {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(input.readLine());
+
+    }
+}
