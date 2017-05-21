33d8c77f87d83fd1cd7f80bc5a9e6f71afde1d05
BenjaminBLi <li.benjamin.b@gmail.com>
2017-05-21 18:53:44 -0400

    Completed an additional ICS Challenge question

--- src/com/sywood/starbucks/ben/ICSChallengeQuestions/Q7.java
+++ src/com/sywood/starbucks/ben/ICSChallengeQuestions/Q7.java
@@ -3,6 +3,17 @@ package com.sywood.starbucks.ben.ICSChallengeQuestions;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
+
+
+/*
+Tuned using quarter-comma mean tone tuning
+a seventeenth is: 5
+a major third is: 5/4
+a fifth is : 5^(0.25), since a seventeenth is 4 stacked fifths
+a whole tone is: sqrt(5)/2.0: two fifths up and an octave down
+a semitone is: 2^3/pow(5, 5/4.0): three octaves up, five fifths down
+ */
+
 public class Q7 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -12,18 +23,20 @@ public class Q7 {
         double root = Math.pow(5, 0.25);
         double f = 1;
         out.printf("Root: %f\n", base*f);
-        f = root*root/2.0;
+        f = Math.pow(root, 2)/2.0;
         out.printf("Maj. 2: %f\n", base*f);
-        f = Math.pow(root, 4)*Math.pow(2, -2);
+        f = Math.pow(root, 4)/4.0;
         out.printf("Maj. 3: %f\n", base*f);
-        f = Math.pow(root, -1)*2;
+        f = Math.pow(root, -1)*2.0;
         out.printf("Per. 4: %f\n", base*f);
         f = root;
         out.printf("Per. 5: %f\n", base*f);
         f = Math.pow(root, 3)/2.0;
         out.printf("Maj. 6: %f\n", base*f);
-        f = Math.pow(root, 5)*Math.pow(2, -2);
-        out.printf("Per. 8: %f\n", base*2);
+        f = Math.pow(root, 5)/4.0;
+        out.printf("Maj. 7: %f\n", base*f);
+        f = 2;
+        out.printf("Per. Octave: %f\n", base*f);
         out.close();
 
     }
