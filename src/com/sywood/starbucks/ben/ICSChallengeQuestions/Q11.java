0f4a2f57ac4038508d9dd40d7452cabfe7133538
BenjaminBLi <li.benjamin.b@gmail.com>
2017-05-21 18:30:30 -0400

    Completed an additional ICS Challenge question

---
+++ src/com/sywood/starbucks/ben/ICSChallengeQuestions/Q11.java
@@ -0,0 +1,53 @@
+package com.sywood.starbucks.ben.ICSChallengeQuestions;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Q11 {
+    static int[][] adj;
+    static int[] dist;
+    static int N;
+    static class Pair implements Comparable<Pair>{
+        int first, second;
+        public Pair(int a, int b) {
+            first = a;
+            second = b;
+        }
+
+        public int compareTo(Pair o) {
+            if(o.first == first) return second < o.second ? -1 : 1;
+            return first < o.first ? -1 : 1;
+        }
+    }
+
+    static int sssp(int st, int en) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>(Collections.reverseOrder());
+        pq.add(new Pair(0, st));
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[st] = 0;
+        while(!pq.isEmpty()) {
+            int u = pq.poll().second;
+            for (int v = 0; v < N; v++) {
+                if(dist[u]+adj[u][v] < dist[v]){
+                    dist[v] = dist[u]+adj[u][v];
+                    pq.add(new Pair(dist[v], v));
+                }
+            }
+        }
+
+        return dist[en];
+    }
+
+    public static void main(String[] args){
+        N = 5;
+        adj = new int[][]{
+                {0, 12, 3, 17, 4},
+                {39, 0, 48, 2, 6},
+                {94, 8, 0, 6, 7},
+                {5, 7, 4, 0, 82},
+                {9, 23, 5, 20, 0}};
+        dist = new int[N];
+        System.out.println(sssp(0, 2));
+    }
+}
