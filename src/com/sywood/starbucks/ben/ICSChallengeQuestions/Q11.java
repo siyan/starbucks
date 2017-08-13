5355414fb434b837a60e68aa06001431c927ce09
BenjaminBLi <li.benjamin.b@gmail.com>
2017-08-13 19:52:50 -0400

    Completed an additional ICS Challenge question

--- src/com/sywood/starbucks/ben/ICSChallengeQuestions/Q11.java
+++ src/com/sywood/starbucks/ben/ICSChallengeQuestions/Q11.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.PriorityQueue;
 
-public class Q11 {
+public class Q11{
     static int[][] adj;
     static int[] dist;
     static int N;
@@ -22,20 +22,24 @@ public class Q11 {
     }
 
     static int sssp(int st, int en) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
         pq.add(new Pair(0, st));
-        Arrays.fill(dist, Integer.MAX_VALUE);
+        Arrays.fill(dist, 0x3f3f3f);
         dist[st] = 0;
+        System.out.println(Arrays.toString(dist));
         while(!pq.isEmpty()) {
             int u = pq.poll().second;
+            System.out.println("CURRENTLY AT NODE: " + u);
             for (int v = 0; v < N; v++) {
                 if(dist[u]+adj[u][v] < dist[v]){
                     dist[v] = dist[u]+adj[u][v];
+                    System.out.println("    ADDING : {" + dist[v] + ", " + v + "} TO THE QUEUE");
                     pq.add(new Pair(dist[v], v));
                 }
             }
+            System.out.println(Arrays.toString(dist)+"\n");
         }
-
+        System.out.println(Arrays.toString(dist));
         return dist[en];
     }
 
